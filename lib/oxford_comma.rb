eebe97971da6758d29e2c56bc198446411272ecf
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-25 02:56:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ def oxford_comma(array)
   if array.length == 1
     array[0]
   elsif array.length == 2
-    array.join(" and")
+    array.join(" and ")
   else
     last_word = array.pop
     list = "#{array.join(", ")}, and #{last_word}"
-  end  
+  end
 end
