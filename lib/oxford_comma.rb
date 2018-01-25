b2382cd931f0d1972cf2337c96c4890fd5b8bd74
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-25 02:58:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    last_word = array.pop
+    list = "#{array.join(", ")}, and #{last_word}"
+  end
 end
